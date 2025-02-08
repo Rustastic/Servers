87a7bd89cbbd990c02b59c95bfe9ff23c1f342b4
ilBuso <alessandro.busola@gmail.com>
2025-02-08 22:26:20 +0100

    made comunication_server public

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 use crate::server::Server;
 
 mod server;
-mod communication_server;
+pub mod communication_server;
 mod packet_cache;
 mod send_functions;
-
-fn main() {
-    println!("Hello, world!");
-}
